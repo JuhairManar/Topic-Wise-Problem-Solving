aa8c18236e75ac73afee74a43567638f78675190
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-06-24 23:52:33 +0600

    Create solution.cpp

---
+++ Number Theory/Codeforces B. Two Divisors/solution.cpp
@@ -0,0 +1,37 @@
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void solve(){
+    ll x,y;
+    cin>>x>>y;
+
+    if(y%x==0){
+        cout<<((y/x)*y);
+    }
+    else{
+        cout<<(x*y)/(__gcd(x,y));
+    }   
+    cout<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+                                        
+     int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+
+  return 0;
+}
