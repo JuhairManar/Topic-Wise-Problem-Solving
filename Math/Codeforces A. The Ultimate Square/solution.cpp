94df8f447933700b90001791268620523541d9ee
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-11-23 00:00:01 +0600

    Create solution.cpp

---
+++ Math/Codeforces A. The Ultimate Square/solution.cpp
@@ -0,0 +1,34 @@
+// link - https://codeforces.com/problemset/problem/1748/A
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve(){
+   ll n,i;       
+    cin>>n;
+    cout<<(n+1)/2<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+
+  return 0;
+}
+
+4 7
