4d9a6e62446b98a4ff7a8de21ad123bbf3468a67
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-11-24 23:58:49 +0600

    Create solution.cpp

---
+++ Bit Manipulation/Codeforces Fun Game/solution.cpp
@@ -0,0 +1,43 @@
+// link - https://codeforces.com/problemset/problem/1994/B
+ 
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve(){
+   ll n,i;       
+   cin>>n;
+   string s,t;
+   cin>>s>>t;
+   
+   for(i=0;i<n && s[i]!='1';i++){
+    if(t[i]=='1'){
+        cout<<"NO\n";
+        return;
+    }
+   }
+   cout<<"YES\n";
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+
+  return 0;
+}
