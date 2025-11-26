29449fce8932290b707bc7c3e42dc133ffdf3e48
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-11-26 23:54:11 +0600

    Create solution.cpp

---
+++ Bit Manipulation/Codechef Xor Again/solution.cpp
@@ -0,0 +1,38 @@
+// link - https://www.codechef.com/problems/XORAGN
+
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve(){
+   ll n,i,ans=0,d;       
+    cin>>n;
+    for(i=0;i<n;i++)   {
+        cin>>d;
+        ans=(ans^(2*d));
+    }
+
+    cout<<ans<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+
+  return 0;
+}
