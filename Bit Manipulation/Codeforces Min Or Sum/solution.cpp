62ef7c46159480fc3de6c8a7384523322848da8a
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-11-27 23:44:01 +0600

    Create solution.cpp

---
+++ Bit Manipulation/Codeforces Min Or Sum/solution.cpp
@@ -0,0 +1,40 @@
+// link- https://codeforces.com/problemset/problem/1635/A
+
+
+/* 
+
+*/
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve(){
+   ll n,d,i,ans=0;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>d;
+        ans|=d;
+    }
+    cout<<ans<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+
+  return 0;
+}
