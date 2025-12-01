bc10f4ab2569a9f05a95d7119a6cfbdd9ddec041
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-12-01 23:58:19 +0600

    Create solution.cpp

---
+++ Math/geometry/Codechef Diagonal movement/solution.cpp
@@ -0,0 +1,39 @@
+// link - https://www.codechef.com/problems/DIAGMOVE
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define pb push_back
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve(){
+   ll n,i,s=0;       
+   cin>>n;
+   vector<ll>v(n);
+   for(auto &g:v){
+    cin>>g;
+    s+=g;
+   } 
+   sort(v.begin(),v.end());
+
+   cout<<(2*s)-v[0]-v[1]<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int t;
+    cin>>t;
+    while(t--) solve();
+
+
+  return 0;
+}
